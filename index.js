46a3d9dd7c75fce4fce12473c761915ea71d9092
Incinscible <incinscible@gmail.com>
2023-02-18 12:41:20 +0100

    modif cors

--- index.js
+++ index.js
@@ -14,7 +14,9 @@ const app = express();
 const port = 3000;
 
 app.use(bodyParser.json());
-app.use(cors());
+app.use(cors({
+  origin: '*'
+}));
 
 // Protect all /locations route with JWT Authentication
 app.use(
